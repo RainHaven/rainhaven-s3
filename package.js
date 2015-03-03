1b5b6f03520dc2c75c8cba1ae435dfcc611927c7
Scott McPherson <scott_fl@me.com>
2015-03-03 11:31:42 -0500

    update versions

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'rainhaven:s3',
   summary: 'S3 and knox for Meteor',
   version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  git: 'https://github.com/RainHaven/rainhaven-s3.git'
 });
 
 Package.onUse(function(api) {
