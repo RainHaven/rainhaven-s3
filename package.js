e55524db1a55d181ef38c891ec449b0ee7fbc837
Scott McPherson <scott_fl@me.com>
2015-03-04 10:34:10 -0500

    bump to version 1.0.1
    added example and removed void keys

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'rainhaven:s3',
   summary: 'S3 and knox for Meteor',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/RainHaven/rainhaven-s3.git'
+});
+
+Npm.depends({
+  knox: "0.9.2"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
-
-  Npm.depends({
-    knox: "0.9.2"
-  });
 
   api.addFiles('client/s3.js', 'client');
   api.addFiles('server/s3.js', 'server');
